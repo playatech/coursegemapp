30c79889c2c157a69e9a2c2ee9b58fda109f4f17
Playatech <playa.tech.in@gmail.com>
2020-08-19 16:35:22 +0000

    Added gem ransack - for users.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,10 @@
 class UsersController < ApplicationController
   def index
-    @users = User.all.order(created_at: :desc)
+    # @users = User.all.order(created_at: :desc)
+    # before ^ and after v Ransack
+    
+    @q = User.ransack(params[:q])
+    @users = @q.result(distinct: true)
+  
   end
 end
